fc49e798ceb3b13460ce5c373cb56831ffbd27fd
hager-dot <hagerelhwarii6633@gmail.com>
2023-10-13 15:36:45 +0300

    :sparkles: Update the speed of the slide effect

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-
 $(function () {
   $(".sub-btn").on("click", function () {
-    // To display sub-menu and then hide it. 
-    $(this).next(".sub-menu").slideToggle('linear');
+    // To display sub-menu and then hide it.
+    $(this).next(".sub-menu").slideToggle("swing");
 
     // To Rotate right arrow or sub-btn when displaying sub-menu.
-    $(this).find('.dropdown').toggleClass('rotate')
+    $(this).find(".dropdown").toggleClass("rotate");
   });
 
-   // To expand and collapse the sidebar.
-  $('.menu-btn').on('click',function(){
-     $('.sidebar').addClass('active');
-     $('.menu-btn').css('visibility','hidden')
-  })
-  $('.close-btn').on('click',function(){
-    $('.sidebar').removeClass('active');
-    $('.menu-btn').css('visibility','visible')
- })
-
+  // To expand and collapse the sidebar.
+  $(".menu-btn").on("click", function () {
+    $(".sidebar").addClass("active");
+    $(".menu-btn").css("visibility", "hidden");
+  });
+  $(".close-btn").on("click", function () {
+    $(".sidebar").removeClass("active");
+    $(".menu-btn").css("visibility", "visible");
+  });
 });
