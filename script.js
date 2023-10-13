2ed2938355a4795e33b39ee2af52ffc52a745e1e
hager-dot <hagerelhwarii6633@gmail.com>
2023-10-13 15:34:18 +0300

    :sparkles: Finish sidebar menu

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(function () {
+  $(".sub-btn").on("click", function () {
+    // To display sub-menu and then hide it. 
+    $(this).next(".sub-menu").slideToggle('linear');
+
+    // To Rotate right arrow or sub-btn when displaying sub-menu.
+    $(this).find('.dropdown').toggleClass('rotate')
+  });
+
+   // To expand and collapse the sidebar.
+  $('.menu-btn').on('click',function(){
+     $('.sidebar').addClass('active');
+     $('.menu-btn').css('visibility','hidden')
+  })
+  $('.close-btn').on('click',function(){
+    $('.sidebar').removeClass('active');
+    $('.menu-btn').css('visibility','visible')
+ })
+
+});
